ccf48ffff31de6d61febfc73b6091401f34b2d58
Alvareto <ivan.grgurina@gmail.com>
2013-01-28 11:31:14 -0800

    Create main.h

---
+++ main.h
@@ -0,0 +1,5 @@
+void sjecPravaca (float a1, float b1, float a2, float b2);
+void troznBroj (int x);
+void povrsKruga (float radijus);
+void kvadJedn (double a, double b, double c);
+void djeljivBroj (int a, int b);
